e8c7a4cc800433ed0a351281cea6770292028271
liamcripwell <ljcripwell@gmail.com>
2021-09-03 22:18:47 +0200

    keep those not in val for train

--- doc_simp/models/classification.py
+++ doc_simp/models/classification.py
@@ -213,7 +213,7 @@ class BertDataModule(pl.LightningDataModule):
             print("Loading specific validation samples...")
             val_file = pd.read_csv(self.val_file)
             self.validate = self.data.loc[val_file.idx]
-            self.data = self.data[self.data.index.isin(val_file.idx)]
+            self.data = self.data[~self.data.index.isin(val_file.idx)]
         self.data = self.data.sample(frac=1)[:self.max_samples] # NOTE: this will actually exclude the last item
         print("All data loaded.")
 
