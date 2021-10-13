4bcbd1a74a60fecdd83f46a8757e3ad832d555e6
liamcripwell <ljcripwell@gmail.com>
2021-10-13 15:47:29 +0200

    dont do actual generation in generative_step

--- doc_simp/models/end_to_end.py
+++ doc_simp/models/end_to_end.py
@@ -143,31 +143,31 @@ class BartFinetuner(pl.LightningModule):
         input_ids, attention_mask, labels = batch
 
         # generate sequences from batch input
-        generated_ids = self.model.generate(
-            input_ids,
-            attention_mask=attention_mask,
-            use_cache=True,
-            decoder_start_token_id=self.decoder_start_token_id,
-            num_beams=self.eval_beams,
-            max_length=self.eval_max_length,
-        )
-        gen_time = (time.time() - t0) / input_ids.shape[0]
-        preds: List[str] = self.ids_to_clean_text(generated_ids)
-        target: List[str] = self.ids_to_clean_text(labels)
+        # generated_ids = self.model.generate(
+        #     input_ids,
+        #     attention_mask=attention_mask,
+        #     use_cache=True,
+        #     decoder_start_token_id=self.decoder_start_token_id,
+        #     num_beams=self.eval_beams,
+        #     max_length=self.eval_max_length,
+        # )
+        # gen_time = (time.time() - t0) / input_ids.shape[0]
+        # preds: List[str] = self.ids_to_clean_text(generated_ids)
+        # target: List[str] = self.ids_to_clean_text(labels)
 
         # compute loss
         loss_tensors = self._step(batch)
         base_metrics = {name: loss for name, loss in zip(self.loss_names, loss_tensors)}
 
         # calculate other metrics
-        bleu: Dict = self.calc_generative_metrics(preds, target)
-        summ_len = np.mean(lmap(len, generated_ids))
-        base_metrics.update(
-            gen_time=gen_time,
-            gen_len=summ_len,
-            preds=preds,
-            target=target,
-            **bleu)
+        # bleu: Dict = self.calc_generative_metrics(preds, target)
+        # summ_len = np.mean(lmap(len, generated_ids))
+        # base_metrics.update(
+            # gen_time=gen_time,
+            # gen_len=summ_len,
+            # preds=preds,
+            # target=target,
+            # **bleu)
 
         return base_metrics
 
