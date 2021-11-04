5efe9e40449b3975cd01740ea26bc4f22ffca938
liamcripwell <ljcripwell@gmail.com>
2021-11-04 12:56:01 +0100

    dont make data_file a required arg

--- control_simp/models/end_to_end.py
+++ control_simp/models/end_to_end.py
@@ -247,7 +247,7 @@ class BartFinetuner(pl.LightningModule):
         parser.add_argument("--lr_scheduler", action="store_true")
         parser.add_argument("--batch_size", type=int, default=16)
         parser.add_argument("--train_workers", type=int, default=8)
-        parser.add_argument("--data_file", type=str, default=None, required=True)
+        parser.add_argument("--data_file", type=str, default=None, required=False)
         parser.add_argument("--max_samples", type=int, default=-1)
         parser.add_argument("--train_split", type=float, default=0.9)
         parser.add_argument("--val_split", type=float, default=0.05)
