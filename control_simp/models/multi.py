ecafc121bef19f7573a648cad0db2ddd5ca142e6
liamcripwell <ljcripwell@gmail.com>
2021-12-09 11:22:33 +0100

    move clf fwd to own method and shift input_ids right before putting in encoder

--- control_simp/models/multi.py
+++ control_simp/models/multi.py
@@ -85,6 +85,8 @@ class BartMultiHead(BartForConditionalGeneration):
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         # GENERATION TASK
+        input_ids_clone = input_ids.clone()
+
         if y_seqs is not None:
             # shift y tokens to the right
             if decoder_input_ids is None and decoder_inputs_embeds is None:
@@ -137,13 +139,50 @@ class BartMultiHead(BartForConditionalGeneration):
         if not do_clf:
             return lm_out
 
+        clf_out = self.clf_forward(
+            input_ids_clone, # not sure if we need clone but ehh
+            attention_mask=attention_mask,
+            encoder_outputs=outputs.encoder_hidden_states
+            #
+        )
+
+        return lm_out, clf_out
+
+
+    def clf_forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        decoder_input_ids=None,
+        decoder_attention_mask=None,
+        decoder_head_mask=None,
+        cross_attn_head_mask=None,
+        encoder_outputs=None,
+        past_key_values=None,
+        decoder_inputs_embeds=None,
+        labels=None,
+        use_cache=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None
+    ):
+
         # CLASSIFICATION TASK
+
+        # different to other models, Bart automatically creates decoder_input_ids from
+        # input_ids if no decoder_input_ids are provided
+        # NOTE: this is automatically done in self.model() but we explicity do it here
+        # so we can pass it through to just self.decoder() for the clf task
+        if decoder_input_ids is None and decoder_inputs_embeds is None:
+            decoder_input_ids = shift_tokens_right(
+                input_ids, self.config.pad_token_id, self.config.decoder_start_token_id
+            )
 
         # rerun decoder with original sentence as input (clf task)
         clf_decoder_outputs = self.model.decoder(
-            input_ids=input_ids,
+            input_ids=decoder_input_ids,
             attention_mask=decoder_attention_mask,
-            encoder_hidden_states=outputs.encoder_hidden_states,
+            encoder_hidden_states=encoder_outputs, #outputs.encoder_hidden_states,
             encoder_attention_mask=attention_mask,
             head_mask=decoder_head_mask,
             cross_attn_head_mask=cross_attn_head_mask,
@@ -196,4 +235,4 @@ class BartMultiHead(BartForConditionalGeneration):
             logits=logits,
         )
 
-        return lm_out, clf_out
+        return clf_out
