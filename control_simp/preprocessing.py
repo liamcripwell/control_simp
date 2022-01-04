896605b0e26a0da20c4ef1156542f1de8a6b1a8d
liamcripwell <ljcripwell@gmail.com>
2022-01-04 09:43:49 +0100

    allow for multi-ref silver labelling

--- control_simp/preprocessing.py
+++ control_simp/preprocessing.py
@@ -34,27 +34,39 @@ def add_labels(df, x_col="complex", y_col="simple", sent_det=None):
     for i, row in df.iterrows():
         label = None
 
-        # split simple into sents
-        if sent_det is None:
-            sents = sent_tokenize(row[y_col])
+        if y_col.isinstance(list):
+            ys = [row[y] for y in y_col]
         else:
-            sents = row[y_col].split(sent_det)
-        
-        # determine label
-        if len(sents) == 1:
-            if "lev_ratio" not in row:
-                raise AttributeError("Data needs 'lev_ratio' column to assign labels. Use the `add_lev_ratio()` function to do so.")
-            if row.lev_ratio == 1.0:
-                label = 0
+            ys = [row[y_col]]
+
+        _labels = []
+        for y in ys:
+            # split simple into sents
+            if sent_det is None:
+                sents = sent_tokenize(y)
             else:
-                label = 1
-        else:
-            if find_adverbial(sents[-1]) is None:
-                label = 2
-            else: 
-                label = 3
+                sents = y.split(sent_det)
+            
+            # determine label
+            if len(sents) == 1:
+                if "lev_ratio" not in row:
+                    raise AttributeError("Data needs 'lev_ratio' column to assign labels. Use the `add_lev_ratio()` function to do so.")
+                if row.lev_ratio == 1.0:
+                    label = 0
+                else:
+                    label = 1
+            else:
+                if find_adverbial(sents[-1]) is None:
+                    label = 2
+                else: 
+                    label = 3
+            
+            _labels.append(label)
         
-        labels.append(label)
+        if len(ys) == 1:
+            labels.append(_labels[0])
+        else:
+            labels.append(label)
     
     df["label"] = labels
 
